f12aa1e23edd1c65c3dffe13ababbd3f2f7fe754
Daniel <dwalker.prsvr@gmail.com>
2023-08-24 15:05:45 -0400

    finishedProduct

---
+++ main.js
@@ -0,0 +1,88 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = 'Daniel'
+let myLastName = 'Walker'
+let myBestFriend = 'Drew'
+let mySentence = myFirstName + " " + myLastName + " isn't " + myBestFriend + "."
+let myFavoriteNum = 256
+let myMentalAge = 36
+let myHeight = 72
+let myFavoriteYear = 2011
+let mySum = 3 + 1918 + 24 + 1
+let myProduct = 232 * 43 / 2 + 4
+let myHeightAndAge = myMentalAge + myHeight
+
+
+
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myBestFriend === "undefined") {
+  myBestFriend = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myFavoriteNum === "undefined") {
+  myFavoriteNum = undefined;
+}
+
+if (typeof myMentalAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myBestFriend,
+  mySentence,
+  myFavoriteNum,
+  myMentalAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
